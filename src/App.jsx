5ed5e557e9626e274f9017e6fe5354b1a4c660b6
Orozon64 <ignacy.guminiak@tm1.edu.pl>
2026-02-27 15:13:42 +0100

    handled user input, added properties info

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,58 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [aCoefficient, setACoefficient] = useState(0)
+  const [bCoefficient, setBCoefficient] = useState(0)
+  function PropertiesInfoBox(){
+      let root, monotonity;
+      if (aCoefficient == 0) {
+        monotonity = "stała";
+        if(bCoefficient == 0){
+          root = "nieskończenie wiele";
+        }
+        else{
+          root = "brak";
+        }
+      }
+      else{
+        if (aCoefficient > 0) {
+          monotonity = "rosnąca";
+        }
+        else{
+          monotonity = "malejąca";
+        }
+        root = -bCoefficient/aCoefficient;
+      }
+      return(
+        <div>
+          <p>Miejsce zerowe: {root}</p>
+          <p>Punkt przecięcia z OY: (0, {bCoefficient})</p>
+          <p>Monotoniczność: {monotonity}</p>
+        </div>
+      )
+    
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Kalkulator funkcji liniowych</h1>
+      <p>Pamiętaj: ułamki dziesiętne wpisuj z kropkami, nie z przecinkami</p>
+        <div className='form-group'>
+          <label htmlFor='a-input'>Podaj współczynnik a:</label>
+          <input className='form-control' id='a-input' value={aCoefficient} type='number' onChange={(e)=>{setACoefficient(e.target.value)}}></input>
+        </div>
+        <div className='form-group'>
+          <label htmlFor='b-input'>Podaj współczynnik b:</label>
+          <input className='form-control' id='b-input' value={bCoefficient} type='number' onChange={(e)=>{setBCoefficient(e.target.value)}}></input>
+        </div>
+      
+      <PropertiesInfoBox>
+
+      </PropertiesInfoBox>
     </>
   )
 }
